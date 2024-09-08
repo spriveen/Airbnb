7dae2e53ba208f92619bf6745a8e48a6b7de82e5
spriveen <rpeanuka@gmail.com>
2024-09-08 09:11:27 -0700

    update logo.png

--- app/components/Container.tsx
+++ app/components/Container.tsx
@@ -1,8 +1,18 @@
+'use client';
+interface ContainerProps{
+  children: React.ReactNode;
+}
 
-
-const Container = () => {
+const Container: React.FC<ContainerProps> = ({
+  children
+}) => {
   return (
-    <div>Container</div>
+    <div className="max-w-[2520px] mx-auto
+    xl:px-20 md:px-10 sm:px-20 px-4">
+      
+      
+      {children}
+      </div>
   )
 }
 
